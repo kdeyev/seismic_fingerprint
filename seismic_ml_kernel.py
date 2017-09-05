3b8fd8762dc9c763b22b2b01ff346b38f8a4b200
kdeyev <k.deyev@gmial.com>
2017-09-05 07:36:35 +0300

    concatenated model: the model is based on 3 vision models: data, fk, and spectrum and one classification model

--- seismic_ml_kernel.py
+++ seismic_ml_kernel.py
@@ -103,6 +103,95 @@ def ci (X_train, y_train, X_test, y_test, num_epochs = 200):
 			  batch_size=batch_size, nb_epoch=num_epochs,
 			  verbose=1, validation_split=0.1) # ...holding out 10% of the data for validation
 	print(model.evaluate(X_test, Y_test, verbose=1)) # Evaluate the trained model on the test set!
+
+def ci_vision_model (X_train):
+	from keras.models import Model # basic class for specifying and training a neural network
+	from keras.layers import Input, Convolution2D, MaxPooling2D, Dense, Dropout, Flatten
+	
+	kernel_size = 3 # we will use 3x3 kernels throughout
+	pool_size = 2 # we will use 2x2 pooling throughout
+	conv_depth_1 = 32 # we will initially have 32 kernels per conv. layer...
+	conv_depth_2 = 64 # ...switching to 64 after the first pooling layer
+	drop_prob_1 = 0.25 # dropout after pooling with probability 0.25
+	
+	num_train, depth, height, width = X_train.shape # there are 50000 training examples in CIFAR-10 
+	
+	print ('image has', X_train.shape[1]*X_train.shape[2], 'pixels')
+	
+	inp = Input(shape=(depth, height, width)) # N.B. depth goes first in Keras!
+	# Conv [32] -> Conv [32] -> Pool (with dropout on the pooling layer)
+	conv_1 = Convolution2D(conv_depth_1, kernel_size, kernel_size, border_mode='same', activation='relu')(inp)
+	conv_2 = Convolution2D(conv_depth_1, kernel_size, kernel_size, border_mode='same', activation='relu')(conv_1)
+	pool_1 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_2)
+	drop_1 = Dropout(drop_prob_1)(pool_1)
+	# Conv [64] -> Conv [64] -> Pool (with dropout on the pooling layer)
+	conv_3 = Convolution2D(conv_depth_2, kernel_size, kernel_size, border_mode='same', activation='relu')(drop_1)
+	conv_4 = Convolution2D(conv_depth_2, kernel_size, kernel_size, border_mode='same', activation='relu')(conv_3)
+	pool_2 = MaxPooling2D(pool_size=(pool_size, pool_size))(conv_4)
+	drop_2 = Dropout(drop_prob_1)(pool_2)
+	# Now flatten to 1D, apply FC -> ReLU (with dropout) -> softmax
+	flat = Flatten()(drop_2)
+
+	vision_model = Model(input=inp, output=flat) # To define a model, just specify its input and output layers
+	vision_model_inp = inp
+	vision_model_out = vision_model (inp)
+	return vision_model_inp, vision_model_out
+	
+	
+def ci_multi (X_train, y_train, X_test, y_test, num_epochs = 200):
+	from keras.models import Model # basic class for specifying and training a neural network
+	from keras.layers import Input, Convolution2D, MaxPooling2D, Dense, Dropout, Flatten
+	from keras.utils import np_utils # utilities for one-hot encoding of ground truth values
+	import numpy as np
+	import keras
+		
+	num_test = X_test[0].shape[0] # there are 10000 test examples in CIFAR-10
+	num_classes = np.unique(y_train).shape[0] # there are 10 image classes
+	
+	print ('num branches', len (X_train))
+	
+	batch_size = 32 # in each iteration, we consider 32 training examples at once
+	#num_epochs = 200 # we iterate 200 times over the entire training set
+	hidden_size = 512 # the FC layer will have 512 neurons
+	drop_prob_2 = 0.5 # dropout in the FC layer with probability 0.5
+		
+	vision_model_inputs = []
+	vision_model_outputs = []
+	for i in range(len(X_train)):
+		X_train[i] = X_train[i].astype('float32') 
+		X_test[i] = X_test[i].astype('float32')
+		X_train[i] /= np.max(X_train[i]) # Normalise data to [0, 1] range
+		X_test[i]  /= np.max(X_train[i]) # Normalise data to [0, 1] range
+		vision_model_inp, vision_model_out = ci_vision_model (X_train[i])
+		vision_model_inputs.append(vision_model_inp)
+		vision_model_outputs.append(vision_model_out)
+		
+	print ('Vision models are ready')
+
+	Y_train = np_utils.to_categorical(y_train, num_classes) # One-hot encode the labels
+	Y_test = np_utils.to_categorical(y_test, num_classes) # One-hot encode the labels
+	
+	concatenated_inp = keras.layers.concatenate(vision_model_outputs)	
+	hidden = Dense(hidden_size, activation='relu')(concatenated_inp)
+	drop_3 = Dropout(drop_prob_2)(hidden)
+	out = Dense(num_classes, activation='softmax')(drop_3)
+
+	classification_model = Model(vision_model_inputs, out)
+
+	print ('Classification model is ready')
+	
+	classification_model.compile(loss='categorical_crossentropy', # using the cross-entropy loss function
+				  optimizer='adam', # using the Adam optimiser
+				  metrics=['accuracy']) # reporting the accuracy
+
+	print ('Classification model compiled')
+	
+	classification_model.fit(X_train, Y_train, # Train the model using the training set...
+			  batch_size=batch_size, nb_epoch=num_epochs,
+			  verbose=1, validation_split=0.1) # ...holding out 10% of the data for validation
+			  
+	print(classification_model.evaluate(X_test, Y_test, verbose=1)) # Evaluate the trained model on the test set!
+
 	
 def ci_speed (X_train, y_train, X_test, y_test):
 	from keras.datasets import mnist # subroutines for fetching the MNIST dataset
